c68c14d952780cdb87ca1ff75994f23dd185d036
memo <sirmemogarcia@gmail.com>
2015-10-12 16:10:52 +0100

    Quick Fix: Date Time Picker support for 24 hours format
    Change-Id: I1880f0ac1fd834d3f79cc42ec431353a1f9d8343

--- freezer_ui/static/freezer/js/freezer.datetimepicker.js
+++ freezer_ui/static/freezer/js/freezer.datetimepicker.js
@@ -4,10 +4,10 @@
 
 $(function () {
     $('#id_schedule_start_date').datetimepicker({
-        format: 'YYYY-MM-DDThh:mm:ss'
+        format: 'YYYY-MM-DDTHH:mm:ss'
     });
 
     $('#id_schedule_end_date').datetimepicker({
-        format: 'YYYY-MM-DDThh:mm:ss'
+        format: 'YYYY-MM-DDTHH:mm:ss'
     });
 });
