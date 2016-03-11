a1541e6a5992278dc24d3c30222126a205c0d9a0
Memo Garcia <sirmemogarcia@gmail.com>
2016-03-11 15:01:51 +0000

    UX improvements
    Fix calendar to better display in small windows
    Bump the number of results for listing items
    Quick workaround to successfully listing items after being created
    
    Closes bug: 1556939
    Change-Id: Ibaf0a999a729197900b18c1f22fd819f5e11e797

--- disaster_recovery/static/freezer/js/freezer.actions.advanced.js
+++ disaster_recovery/static/freezer/js/freezer.actions.advanced.js
@@ -18,12 +18,6 @@
 
 "use strict";
 
-function hideIncrementalOptions () {
-    $("#id_max_level").closest(".form-group").hide();
-    $("#id_always_level").closest(".form-group").hide();
-    $("#id_restart_always_level").closest(".form-group").hide();
-}
-
 $("#id_no_incremental").click(function () {
     if ($("#id_no_incremental").is(":checked")) {
         $("#id_max_level").closest(".form-group").hide();
@@ -35,6 +29,3 @@ $("#id_no_incremental").click(function () {
         $("#id_restart_always_level").closest(".form-group").show();
     }
 });
-
-
-hideIncrementalOptions();
